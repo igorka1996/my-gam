5eaa6852c18fe2b61cbf14ab7371241e2feaa5b8
Igor <igazarov@mail.ru>
2022-06-15 21:34:29 +0300

    Commit - 1

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <p>Loremeferb 33gb3rb34b</p>
         <a
           className="App-link"
           href="https://reactjs.org"
